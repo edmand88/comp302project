ba505fe1c9bc82420a6e0a494ea1864daa88dd97
Izanyoi <145055869+Izanyoi@users.noreply.github.com>
2024-11-12 15:51:05 -0500

    list_to_bits and get_huffman_codes

--- HuffmanCodes.ml
+++ HuffmanCodes.ml
@@ -18,7 +18,6 @@ let makePriorityQueue =
       match next with
       | [] -> queue:= (!queue)@[toAdd]
       | hd::tl ->
-          (*Modify this for a more generic compare function*)
           if toAdd < hd then
             queue:= (prev@[toAdd]@next)
           else
@@ -41,3 +40,42 @@ let makePriorityQueue =
     enqueue = enqueue;
     dequeue = dequeue;
   }
+  
+  
+let count_sort (toCount: 'a list) : ('a * int) list =
+  [] 
+     
+(*TAIL RECURSIVE*)
+let list_to_int bits = 
+  let rec build_int (lst: bool list) (acc: int): int =
+    match lst with
+    | [] -> acc
+    | hd::tl ->
+        match hd with
+        | true -> build_int tl (acc*2 + 1)
+        | false -> build_int tl (acc*2)
+  in
+  build_int bits 0
+
+
+(*NOT TAIL RECURSIVE -> USE CONTINUATIONS*)
+let get_huffman_codes (full_tree: 'a tree) : ('a, int * int) Hashtbl.t=
+  let hash = Hashtbl.create 256 in (*Not sure about the quanity*) 
+    
+  let rec build_codes (bits: bool list) (node: 'a tree)  =
+    match node with
+    | Leaf(_, c) -> Hashtbl.add hash c ((list_to_int bits), List.length bits)
+    | Node(_, left, right) -> 
+        build_codes (false::bits) left; 
+        build_codes (true::bits) right
+  in
+  build_codes [] full_tree;
+  hash;; 
+          
+    
+let huffmanEncode (toEncode: 'a list) : bool list = 
+  (*Might wanna make this variable for the type of data structure this is encoding, if we wanna encode for something else than bytes/chars*)
+  
+  (*We now have a completed tree*)
+  raise NotImplemented
+;; 
